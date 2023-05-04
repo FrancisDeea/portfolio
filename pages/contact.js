498e6e7c83f26eb485f1529a9714dd431e78b641
Francis <francisbernal14@gmail.com>
2023-05-04 23:52:47 +0200

    add metadata

--- pages/contact.js
+++ pages/contact.js
@@ -5,34 +5,36 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { useTranslation } from 'next-i18next'
 
 export default function Contact() {
-    const { t } = useTranslation('contact');
+  const { t } = useTranslation('contact');
 
-    return (
-        <>
-            <Head>
-                <title>Contact Francis</title>
-                <meta name="description" content="Contact to Francis, a junior fullstack developer based in Málaga, Spain" />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-            </Head>
-            <section className={styles.container}>
-                <h2 className={styles.title}>{t('title')}</h2>
-                <p className={styles.p}>{t('description')}</p>
-                <div className={styles.social_container}>
-                    <a href="https://www.linkedin.com/in/francis-bernal-full-stack-developer/" target="_blank"><BsLinkedin className={styles.icon} /></a>
-                    <a href="mailto:francisbernal14@gmail.com"><BsFillEnvelopeFill className={styles.icon} /></a>
-                    <a href="https://github.com/FrancisDeea" target='_blank'><BsGithub className={styles.icon} /></a>
-                </div>
-            </section>
-        </>
-    )
+  return (
+    <>
+      <Head>
+        <title>Francis Bernal Cabra | Contact JavaScript Developer</title>
+        <meta name="description" content="Contact me! I can help to develop your new app on internet!" />
+        <meta name="keywords" content="developer, javascript, app, malaga" />
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <section className={styles.container}>
+        <h2 className={styles.title}>{t('title')}</h2>
+        <p className={styles.p}>{t('description')}</p>
+        <div className={styles.social_container}>
+          <a href="https://www.linkedin.com/in/francis-bernal-full-stack-developer/" target="_blank"><BsLinkedin className={styles.icon} /></a>
+          <a href="mailto:francisbernal14@gmail.com"><BsFillEnvelopeFill className={styles.icon} /></a>
+          <a href="https://github.com/FrancisDeea" target='_blank'><BsGithub className={styles.icon} /></a>
+        </div>
+      </section>
+    </>
+  )
 }
 
 export async function getStaticProps({ locale }) {
-    return {
-      props: {
-        ...(await serverSideTranslations(locale, [
-          'contact'
-        ])),
-      },
-    }
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, [
+        'contact'
+      ])),
+    },
   }
+}
